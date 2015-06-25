21b9dd066d03e8a04438e80ac074d07d2e212e86
Steve Burns <smburns47@gmail.com>
2015-06-25 06:09:25 -0700

    More changes

--- source/DRAMAnalyzer.cpp
+++ source/DRAMAnalyzer.cpp
@@ -27,25 +27,43 @@ void DRAMAnalyzer::WorkerThread()
 	mWb = GetAnalyzerChannelData( mSettings->mWbChannel );
 	mOEb = GetAnalyzerChannelData( mSettings->mOEbChannel );
 
+	bool valid_row_addr = false;
+
 	for( ; ; ) {
 
+	  U64 next_RAS = mRASb->GetSampleOfNextEdge();
+	  U64 next_CAS = mCASb->GetSampleOfNextEdge();
 
-	  if( mRASb->GetBitState() == BIT_HIGH ) {
-	    U64 starting_sample = mRASb->GetSampleNumber();
-	    mRASb->AdvanceToNextEdge();
+	  std::cout << next_RAS << " " << next_CAS << std::endl;
+
+	  bool prev_RASb_is_high = mRASb->GetBitState() == BIT_HIGH;
+	  bool prev_CASb_is_high = mCASb->GetBitState() == BIT_HIGH;
+
+	  if        ( next_RAS == next_CAS) {
+	    mRASb->AdvanceToNextEdge();	    
+	    mCASb->AdvanceToNextEdge();
+	  } else if ( next_RAS < next_CAS) {
+	    mRASb->AdvanceToNextEdge();	    
+
+	    if ( prev_RASb_is_high && prev_CASb_is_high) {
+	      valid_row_addr = true;
+	    }
+
+	  } else { /* next_RAS > next_CAS */
+	    mCASb->AdvanceToNextEdge();
+
+	    U64 starting_sample = mCASb->GetSampleNumber();
 
 	    //we have a byte to save. 
 	    Frame frame;
-	    frame.mData1 = 7; // Hacked
+	    frame.mData1 = 1;
 	    frame.mFlags = 0;
 	    frame.mStartingSampleInclusive = starting_sample;
-	    frame.mEndingSampleInclusive = mRASb->GetSampleNumber();
+	    frame.mEndingSampleInclusive = starting_sample + 1;
 
 	    mResults->AddFrame( frame );
 	    mResults->CommitResults();
 	    ReportProgress( frame.mEndingSampleInclusive );
-	  } else {
-	    mRASb->AdvanceToNextEdge();
 	  }
 
           CheckIfThreadShouldExit();
